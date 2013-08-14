59d827d6dbf379611cbda798cc01f6a7672e225d
Stark Riedesel <starkriedesel@gmail.com>
2013-08-13 23:54:17 -0500

    Added seeds for admin and non-admin users.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create([{username: 'admin', password: 'password', password_confirmation: 'password'},
+             {username: 'user', password: 'password', password_confirmation: 'password'}])
